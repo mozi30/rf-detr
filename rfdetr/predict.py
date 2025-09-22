abae703cd9a0ff43e811a28b47aa3d7dd22e4962
Moritz Zideck <moritz.zideck@gmail.com>
2025-09-22 10:20:50 +0200

    Add local modifications: annotated_image.png and predict.py

---
+++ rfdetr/predict.py
@@ -0,0 +1,12 @@
+import io
+import requests
+import supervision as sv
+from PIL import Image
+from rfdetr import RFDETRBase
+from rfdetr.util.coco_classes import COCO_CLASSES
+
+model = RFDETRBase()
+
+model.train(
+    
+)
